67f6dbdb039654f51cff0b2227c49159dd121948
Tenghy23 <66905172+Tenghy23@users.noreply.github.com>
2023-01-21 14:31:15 +0800

    wip display list of products

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,15 +5,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { ShopModule } from './shop/shop.module';
+import { CoreModule } from './core/core.module';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [AppComponent, NavBarComponent, ProductItemComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    CoreModule,
+    ShopModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
