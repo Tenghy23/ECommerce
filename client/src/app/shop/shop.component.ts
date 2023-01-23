53d498d9df8c94e90ce2c739d47a2a6cde4fd464
Tenghy23 <66905172+Tenghy23@users.noreply.github.com>
2023-01-24 00:16:21 +0800

    wip display list of products 2

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -13,6 +13,10 @@ export class ShopComponent implements OnInit {
   constructor(private shopService: ShopService) {}
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
     this.shopService.getProducts().subscribe((response) => {
       this.products = response.data;
     }),
