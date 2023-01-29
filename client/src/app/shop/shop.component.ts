251b8616907812bf9aaf954d9617f3cd0e77da53
Your Name <you@example.com>
2023-01-30 01:48:08 +0800

    commit the rest of the files

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IBrand } from '../shared/models/brands';
 import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/productType';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -9,19 +11,53 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  brandIdSelected = 0;
+  typeIdSelected = 0;
 
   constructor(private shopService: ShopService) {}
 
   ngOnInit(): void {
     this.getProducts();
+    this.getBrands();
+    this.getTypes();
   }
 
   getProducts() {
-    this.shopService.getProducts().subscribe((response) => {
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe((response) => {
       this.products = response.data;
     }),
       (error) => {
         console.log(error);
       };
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe((response) => {
+      this.brands = [{id: 0, name: 'All'}, ...response];
+    }),
+      (error) => {
+        console.log(error);
+      };
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe((response) => {
+      this.types = [{id: 0, name: 'All'}, ...response];
+    }),
+      (error) => {
+        console.log(error);
+      };
+  }
+  
+  onBrandSelected(brandId: number) {
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number) {
+    this.typeIdSelected = typeId;
+    this.getProducts();
   }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IBrand } from '../shared/models/brands';
+import { IType } from '../shared/models/productType';
 import { IPagination } from '../shared/models/pagination';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +13,32 @@ export class ShopService {
 
   constructor(private http: HttpClient) {}
 
-  getProducts() {
-    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  getProducts(brandId?: number, typeId?: number) {
+    let params = new HttpParams();
+
+    if(brandId) {
+      params = params.append('brandId', brandId.toString());
+    }
+
+    if(typeId) {
+      params = params.append('typeId', typeId.toString());
+    }
+
+    // get an observable from http request
+    // pipe is a wrapper around any rxjs operators u wanna use
+    return this.http.get<IPagination>(this.baseUrl + 'products', {observe: 'response', params})
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      )
+  }
+
+  getBrands() {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
   }
 }
